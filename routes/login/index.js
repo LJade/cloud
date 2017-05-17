3d3b8bdb8592d5bc30baf4fb694792f6503c8f41
LJade <zly1615@qq.com>
2017-05-17 17:32:21 +0800

    [zhou] Vuex

--- routes/login/index.js
+++ routes/login/index.js
@@ -1,7 +1,6 @@
 exports.login = function(req,res,next){
   let username = req.body.username;
   let password = req.body.password;
-  console.log(res.body);
   if(username==='admin' && password==='123456'){
     res.jsonp({status:200,message:'登录成功',data:{token:'012445544'}})
   }else{
